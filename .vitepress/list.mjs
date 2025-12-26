89db5615f0f636b5f276779c22c8eb046d781ae1
tosry_cn <tosrycn@qq.com>
2025-12-26 19:54:58 +0800

    编辑文件 list.mjs

--- .vitepress/list.mjs
+++ .vitepress/list.mjs
@@ -33,6 +33,23 @@ export const list={
         items: [
             {text:"termux设置登陆密码",link: '/post/2025/559'},
             {text: '多开关控制电灯',link: '/post/2025/763' },
+            {text: 'termux常用命令',link: '/post/2025/810' },
+            {text: 'php图片验证码',link: '/post/2025/842' },
+            {text: '数据库便捷预处理',link: '/post/2025/856' },
+            {text: 'php按需加载缓存扩展',link: '/post/2025/858' },
+            {text: '人生真正的需求',link: '/post/2025/861' },
+            {text: '便捷弹出层',link: '/post/2025/877' },
+            {text: 'php中apcu的替代',link: '/post/2025/909' },
+            {text: '宝塔面板开启 HTTP3',link: '/post/2025/913' },
+            {text: 'php设置页面缓存时间',link: '/post/2025/915' },
+            {text: 'js阻止直接跳转外部链接',link: '/post/2025/923' },
+            {text: 'iptables限制连接速率',link: '/post/2025/936' },
+            {text: 'nftable设置防火墙',link: '/post/2025/950' },
+            {text: 'php导出mysql数据库备份',link: '/post/2025/968' },
+            {text: 'url传值中文字符串',link: '/post/2025/969' },
+            {text: '追女成功说',link: '/post/2025/979' },
+            {text: '从穷富三代到真理的孤独',link: '/post/2025/983' },
+            {text: 'php通过订阅生成博友圈',link: '/post/2025/987' },
             {text:"tencentos用tc设置限速",link: '/post/2025/992'},
             {text: 'centos系列ssh安全登陆',link: '/post/2025/994' },
             {text:"人为什么想异性",link: '/post/2025/996'},
@@ -40,8 +57,6 @@ export const list={
             {text:"cnb一键部署到edgeone",link: '/post/2025/1007'},
             {text: '论博客系统框架的自用局限性',link: '/post/2025/1011' },
             {text:"边缘函数操作请求主体",link: '/post/2025/1012'}
-
-
 
             ]
         }
