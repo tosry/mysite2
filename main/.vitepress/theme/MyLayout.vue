23d957273a8ed72cc328758dd9f593f6479be531
tosry_cn <tosrycn@qq.com>
2026-01-01 10:38:38 +0800

    编辑文件 MyLayout.vue

--- main/.vitepress/theme/MyLayout.vue
+++ main/.vitepress/theme/MyLayout.vue
@@ -4,26 +4,23 @@ import { useRoute } from 'vitepress';
 const route = useRoute();
 const issi = ref('');
 const links = {
-  '/si/': '四圣心源',
-  '/yao/': '中药收藏',
-  '/post/2025/': '文章列表(≤2025年)',
-  '/post/2026/': '文章列表(2026年)',
-  '/mi/': '修行(个人收藏，禁止非法传播)'
+  'si': '四圣心源',
+  'yao': '中药收藏',
+  'post': '文章列表',
+  'mi': '修行(保密)'
 };
 const updateIssi = () => {
   const pathname = route.path;
-  for (const [prefix, name] of Object.entries(links)) {
-    if (pathname.startsWith(prefix)) {
-      issi.value = `<a href="${prefix}">${name}</a>`;
-      return;
-    }
-  }
-  issi.value='其它';
+  issi.value=`
+  <span v-for="(segment, index) in path.split('/').slice(1)" :key="index">
+    <a :href="'/' + path.split('/').slice(0, index + 1).join('/')">{{ links[segment]||segment }}</a>
+    <span v-if="index < path.split('/').length - 2"> > </span>
+    </span>`;
 };
 
 watch(() => route.path, updateIssi, { immediate: true });
 
 </script>
 <template>
-      <div class="my-nav-block">当前位置：🏠 <a href="/">首页</a> ｜→｜ <span v-html="issi"></span></div>
+      <div class="my-nav-block">当前位置：🏠 <a href="/">首页</a> > <span v-html="issi"></span></div>
 </template>
