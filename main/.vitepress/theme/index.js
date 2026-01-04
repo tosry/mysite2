5bb19029d6f429c90fc78bf271762ee000841f3e
tosry_cn <tosrycn@qq.com>
2026-01-04 08:55:08 +0800

    编辑文件 index.js

--- main/.vitepress/theme/index.js
+++ main/.vitepress/theme/index.js
@@ -1,5 +1,5 @@
 import { h } from 'vue';
-import DefaultTheme from 'vitepress/theme';
+import DefaultTheme from 'vitepress/theme-without-fonts';
 import MyLayout from './MyLayout.vue';
 import './my-fonts.css';
 
